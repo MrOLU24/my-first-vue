fa48e42b432b455b9ecd8c315cac24a8fabfe495
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-11 13:38:51 +0100

    practing how vue works

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <div id="app">
-    <h1>{{ message }}</h1>
-    <button @click="increment">Count is: {{ count }}</button>
+    <div>
+      <h1>{{ message }}</h1>
+      <button @click="increment">Count is: {{ count }}</button>
+    </div>
+    <div class="contain">
+      <span class="first-span">
+        <span>{{ counter }} </span>
+        * {{ value }} =
+        <span>{{ double }}</span>
+      </span>
+      <div class="btn-div">
+        <button @click="inc">+1</button>
+      <button @click="counter -= 1">-1</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const message = ref("hello world vue 3");
 const count = ref(0);
+const counter = ref(1);
+const value = 2;
+const double = computed(() => counter.value * value);
+function inc() {
+  counter.value++;
+}
 
 function increment() {
   count.value++;
-  message.value = `Clicked ${count.value} Times!`
+  message.value = `Clicked ${count.value} Times!`;
 }
 </script>
 
