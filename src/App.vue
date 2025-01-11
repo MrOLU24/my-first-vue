a77bf3b56967cfc81dcc6327d67dd9a1cf42a81a
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-11 10:41:27 +0100

    my first vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Hello, World!</h1>
+    <p>Welcome to Vue.js with Vite 🚀</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,2 @@
+<script setup>
+import { ref } from 'vue'
